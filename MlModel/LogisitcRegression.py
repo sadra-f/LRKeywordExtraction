14d39915dcddb2eadb34461beca5ef8ecacf2539
Sadra <sadra.f.001@gmail.com>
2025-05-05 00:29:00 +0330

    progress on LR model not finished yet

--- MlModel/LogisitcRegression.py
+++ MlModel/LogisitcRegression.py
@@ -1,11 +1,16 @@
 import numpy as np
 class LogisticRegression:
-    def __init__(self):
+    def __init__(self, steps:int=10000):
         self.weights = None
+        self.steps= steps
     def _standardize(self, data):
-        return data - np.mean(data, axis=0) / np.std(data, axis=0)
+        return (data - np.mean(data, axis=0)) / np.std(data, axis=0)
     def fit(self, data):
-        pass
+        self._initiate_weights()
+        for i in range(self.steps):
+            results = self._sigmoid(np.multiply(data), self.weights)
+    def _initiate_weights(self):
+        self.weights = np.random.random_sample()
     def predict(self, data):
         pass
     def save_weights(self, path):
@@ -13,4 +18,4 @@ class LogisticRegression:
     def load_weights(self, path):
         pass
     def _sigmoid(self, data):
-        return 1 / 1 + np.exp(data)
+        return 1 / (1 + np.exp(data))
