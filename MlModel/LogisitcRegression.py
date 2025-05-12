353592e843b20f00ee0824f6a1ab1574b8820004
Sadra <sadra.f.001@gmail.com>
2025-05-12 19:34:06 +0330

    finished the fitting method

--- MlModel/LogisitcRegression.py
+++ MlModel/LogisitcRegression.py
@@ -1,16 +1,24 @@
 import numpy as np
+import pandas as pd
 class LogisticRegression:
-    def __init__(self, steps:int=10000):
+    def __init__(self, epochs:int=30000, learning_rate:float=0.01):
         self.weights = None
-        self.steps= steps
+        self.epochs= epochs
+        self.learning_rate = learning_rate
     def _standardize(self, data):
         return (data - np.mean(data, axis=0)) / np.std(data, axis=0)
-    def fit(self, data):
-        self._initiate_weights()
-        for i in range(self.steps):
-            results = self._sigmoid(np.multiply(data, self.weights))
-    def _initiate_weights(self):
-        self.weights = np.random.random_sample()
+    def fit(self, X, Y):
+        #TODO: standardize and save standanardized values for later use..
+        X = np.array(X)
+        Y = np.array(self._Check_binary_classes(Y))
+        self._initiate_weights(len(X[0]))
+        for i in range(self.epochs):
+            predictions = self._sigmoid(np.sum(np.multiply(X, self.weights), axis=1))
+            loss = -(Y * np.log(predictions) + (1 - Y) * np.log(1 - predictions))
+            tloss = np.sum(loss)
+            weights = weights - (self.learning_rate / len(X)) * np.dot(X.T, (predictions - Y))
+    def _initiate_weights(self, size):
+        self.weights = np.random.random_sample(size)
     def predict(self, data):
         pass
     def save_weights(self, path):
@@ -18,4 +26,8 @@ class LogisticRegression:
     def load_weights(self, path):
         pass
     def _sigmoid(self, data):
-        return 1 / (1 + np.exp(data))
+        return 1 / (1 + np.exp(-data))
+    def _Check_binary_classes(self, data:pd.DataFrame):
+        if data.dtypes == bool:
+            data = data.astype(int)
+        return data
