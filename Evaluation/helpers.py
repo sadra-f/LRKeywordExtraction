ea31ae3d81eaa22503500c012a9afb65a29bdb37
Sadra <sadra.f.001@gmail.com>
2025-05-18 19:41:08 +0330

    fixed no import bug

---
+++ Evaluation/helpers.py
@@ -0,0 +1,7 @@
+import numpy as np
+def cosine_similarity(a, b):
+    """Compute cosine similarity between two vectors."""
+    dot_product = np.dot(a, b)
+    norm_a = np.linalg.norm(a)
+    norm_b = np.linalg.norm(b)
+    return dot_product / (norm_a * norm_b)
