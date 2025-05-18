2ca622c9469603f367ec60cf5d4323c1c88609f9
Sadra <sadra.f.001@gmail.com>
2025-05-18 19:41:39 +0330

    finished sample test code

--- Run.py
+++ Run.py
@@ -1,28 +1,36 @@
-from Visualize.Dataset import plot
-import pandas as pd
 from MlModel.LogisitcRegression import LogisticRegression as LR
 import numpy as np
-from Evaluation.ClassificationEvaluators import evaluate_classification as ec
 from FeatureExtraction.extractor import text_to_numerical
 from MlModel.BERTBased import BERT_keyword
+from FeatureExtraction.BERTbased import sb_vectorizer as sb
+from Evaluation.helpers import cosine_similarity
 
-with open("test_input.txt", "r") as f:
-    inp_str = f.read()
 
-bert_keys = BERT_keyword(inp_str)
-feature_vector = text_to_numerical(inp_str)
-# remove the word column from the dataset
-cleaned_feature_vector = [i[1:] for i in feature_vector]
+inp_files = ["test_input.txt", "test_input copy.txt"]
+for file in inp_files:
 
-lr = LR()
-lr.load_weights()
-keyword_predictions = lr.predict(cleaned_feature_vector)
+    lr = LR()
+    lr.load_weights()
 
-# put predicted words in a sentence together
-prediction_sent = ""
-for i, val in enumerate(keyword_predictions):
-    if val:
-        prediction_sent += " " + feature_vector[i][0]
-prediction_sent = prediction_sent.strip()
+    with open(file, "r") as f:
+        inp_str = f.read()
+
+    KBERT_keys = BERT_keyword(inp_str)
+    KBERT_keys = [val[0] for val in KBERT_keys]
+    KBERT_key_sent = " ".join(KBERT_keys)
+
+    features = np.array(text_to_numerical(inp_str))
+    # remove the 'word' column from the dataset
+    cleaned_features = np.array(features[:,1:], dtype=np.float64)
+
+    predictions = lr.predict(cleaned_features)
+    keyword_predictions = features[predictions][:,0]
+    # put predicted words in a sentence together
+    predict_key_sent = " ".join(keyword_predictions)
+
+    embeddings = sb([KBERT_key_sent, predict_key_sent])
+    print("\t- KeyBERT Predicted Keywords :\r\n", KBERT_key_sent, "\r\n")
+    print("\t- Custom Predicted Keywords :\r\n", predict_key_sent)
+    print("- Cosine Similarity of Custom and BERT Ceyword Predictions : ", cosine_similarity(embeddings[0], embeddings[1]))
 
 print()
