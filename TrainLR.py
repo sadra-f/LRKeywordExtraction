fac1863c53684fc65301be0b7e21eefb1d38b023
Sadra <sadra.f.001@gmail.com>
2025-07-25 13:45:59 +0330

    modified plotting so True/False class data have differing colors.

--- TrainLR.py
+++ TrainLR.py
@@ -3,12 +3,27 @@ import pandas as pd
 from MlModel.LogisitcRegression import LogisticRegression as LR
 import numpy as np
 from Evaluation.ClassificationEvaluators import evaluate_classification as ec
+from matplotlib import pyplot as plt
 
 ''' use this sample to train the Logistic Regression model with provided dataset '''
 
 # Load dataset
 dataset = pd.read_csv("dataset/numerical_dataset.csv")
 dataset.pop("word")
+columns_to_plot = dataset.iloc[:, 1:13]
+
+# Iterate over the selected columns and plot each one
+for col_name in columns_to_plot.columns:
+    plt.figure(figsize=(10, 5))  # Create a new figure for each column
+    trues = columns_to_plot[dataset["keyword"]==True][col_name]
+    falses = columns_to_plot[dataset["keyword"]==False][col_name]
+    plt.scatter(trues, [i for i in range(len(trues))], color='blue')
+    plt.scatter(falses, [i for i in range(len(falses))], color='red')
+    plt.title(f'Values of {col_name} over Record Number')
+    plt.xlabel('Record Number')
+    plt.ylabel(col_name)
+    plt.grid(True)
+    plt.show()
 # Split dataset to train and test
 t = np.random.rand(len(dataset)) < 0.75
 train = dataset[t]
